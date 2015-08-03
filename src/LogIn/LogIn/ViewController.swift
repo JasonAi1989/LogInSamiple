a0ae18d081ec93479fa5a77e6ec68224a011b167
jason <aizhaoyu@163.com>
2015-08-03 23:35:03 +0800

    bubble animation

--- src/LogIn/LogIn/ViewController.swift
+++ src/LogIn/LogIn/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOutlet weak var Bubble3: UIImageView!
+@IBOutlet weak var Bubble4: UIImageView!
+@IBOutlet weak var Bubble2: UIImageView!
+@IBOutlet weak var Bubble1: UIImageView!
+@IBOutlet weak var Bubble5: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.Bubble1.transform = CGAffineTransformMakeScale(0, 0)
+        self.Bubble2.transform = CGAffineTransformMakeScale(0, 0)
+        self.Bubble3.transform = CGAffineTransformMakeScale(0, 0)
+        self.Bubble4.transform = CGAffineTransformMakeScale(0, 0)
+        self.Bubble5.transform = CGAffineTransformMakeScale(0, 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: nil, animations: {
+            self.Bubble1.transform = CGAffineTransformMakeScale(1, 1)
+            self.Bubble2.transform = CGAffineTransformMakeScale(1, 1)
+
+        }, completion: nil)
+     
+        UIView.animateWithDuration(0.3, delay: 0.1, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: nil, animations: {
+            self.Bubble3.transform = CGAffineTransformMakeScale(1, 1)
+            self.Bubble4.transform = CGAffineTransformMakeScale(1, 1)
+            }, completion: nil)
+        
+        UIView.animateWithDuration(0.3, delay: 0.2, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: nil, animations: {
+            self.Bubble5.transform = CGAffineTransformMakeScale(1, 1)
+            }, completion: nil)
+    }
 
 }
 
