75847cc743f60758e692aa6caa99c31056c58c31
jason <aizhaoyu@163.com>
2015-08-12 22:39:19 +0800

    add scale animation

---
+++ src/LogIn/LogIn/ScaleViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ScaleViewController.swift
+//  LogIn
+//
+//  Created by jason on 15/8/12.
+//  Copyright (c) 2015年 jason. All rights reserved.
+//
+
+import UIKit
+
+class ScaleViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.transform = CGAffineTransformMakeScale(3.0, 3.0)
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
