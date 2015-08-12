a45999e9f7ae3f2b5293f925d79843bca67a3720
jason <aizhaoyu@163.com>
2015-08-12 22:00:59 +0800

    add position animation

---
+++ src/LogIn/LogIn/PositionViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PositionViewController.swift
+//  LogIn
+//
+//  Created by jason on 15/8/12.
+//  Copyright (c) 2015年 jason. All rights reserved.
+//
+
+import UIKit
+
+class PositionViewController: UIViewController {
+
+
+
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var greenSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: {
+            self.redSquare.center.y = self.view.bounds.height - self.redSquare.center.y
+        })
+
+        UIView.animateWithDuration(1, delay: 0.5, options: nil, animations: {
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+            }, completion: { _ in
+                UIView.animateWithDuration(1, animations: {
+                    self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+                    self.blueSquare.center.y = self.view.bounds.height - self.blueSquare.center.y
+                    })
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
