074d11374b241a8ef9ff6bcfdc7b648f36942307
jason <aizhaoyu@163.com>
2015-08-14 19:14:55 +0800

    add spring animation

---
+++ src/LogIn/LogIn/SpringViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SpringViewController.swift
+//  LogIn
+//
+//  Created by jason on 15/8/14.
+//  Copyright (c) 2015年 jason. All rights reserved.
+//
+
+import UIKit
+
+class SpringViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var redSquare: UIView!
+    @IBOutlet weak var greenSquare: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1, animations: {
+            self.blueSquare.center.x = self.view.bounds.width - self.blueSquare.center.x
+        })
+        
+        UIView.animateWithDuration(4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: nil, animations: {
+            self.redSquare.center.x = self.view.bounds.width - self.redSquare.center.x
+        }, completion: nil)
+        
+        UIView.animateWithDuration(4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 2, options: nil, animations: {
+            self.greenSquare.center.x = self.view.bounds.width - self.greenSquare.center.x
+            }, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
